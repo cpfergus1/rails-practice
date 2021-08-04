cebbf685aeb22c66fcbff1b13f5949502f695aef
Connor Ferguson <68167430+cpfergus1@users.noreply.github.com>
2021-08-04 10:07:39 -0600

    advanced login added

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -70,4 +70,8 @@ class UserTest < ActiveSupport::TestCase
     @user.save
     assert @user.email = 'iamupcase@loud.com'
   end
+
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?('')
+  end
 end
