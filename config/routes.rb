ed909424ceec66582d45ee6a7aaec56d33d243f7
Connor Ferguson <68167430+cpfergus1@users.noreply.github.com>
2021-08-05 11:29:55 -0600

    added password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
   get 'help' => 'static_pages#help'
   get 'about' => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
@@ -6,6 +8,7 @@ Rails.application.routes.draw do
 
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
 
   root 'static_pages#home'
 
